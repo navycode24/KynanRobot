11986a79edb02f0fb9c54af940d172f69c73ef3a
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-07 01:36:39 +0700

    Update __init__.py

--- KynanRobot/__init__.py
+++ KynanRobot/__init__.py
@@ -8,6 +8,13 @@ from aiohttp import ClientSession
 from pyrogram import Client, errors
 from pymongo import MongoClient
 from telethon import TelegramClient
+from telethon.sessions import StringSession
+from telethon.sessions import MemorySession
+from pyrogram.types import Message
+from pyrogram import Client, errors
+from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid, ChannelInvalid
+from pyrogram.types import Chat, User
+from ptbcontrib.postgres_persistence import PostgresPersistence
 
 StartTime = time.time()
 
