88203c959ca11a65c782f2b4b5af4a0b830e041b
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-06 23:47:39 +0700

    Update afk.py

--- KynanRobot/modules/afk.py
+++ KynanRobot/modules/afk.py
@@ -140,22 +140,6 @@ def check_afk(update, context, user_id, fst_name, userc_id):
             update.effective_message.reply_text(res, parse_mode="html")
 
 
-__help__ = """
-*Away from group*
- ᐉ /afk <reason>*:* mark yourself as AFK(away from keyboard).
- ᐉ brb <reason>*:* same as the afk command - but not a command.
-When marked as AFK, any mentions will be replied to with a message to say you're not available!
-"""
-
-__mod_name__ = "ᴀғᴋ​"
-__command_list__ = ["afk"]
-__handlers__ = [
-    (AFK_HANDLER, AFK_GROUP),
-    (AFK_REGEX_HANDLER, AFK_GROUP),
-    (NO_AFK_HANDLER, AFK_GROUP),
-    (AFK_REPLY_HANDLER, AFK_REPLY_GROUP),
-]
-
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
 AFK_REGEX_HANDLER = DisableAbleMessageHandler(
     Filters.regex(r"^(?i)brb(.*)$"), afk, friendly="afk"
@@ -169,3 +153,19 @@ dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
 
+__help__ = """
+*Away from group*
+ ᐉ /afk <reason>*:* mark yourself as AFK(away from keyboard).
+ ᐉ brb <reason>*:* same as the afk command - but not a command.
+When marked as AFK, any mentions will be replied to with a message to say you're not available!
+"""
+
+
+__mod_name__ = "ᴀғᴋ​"
+__command_list__ = ["afk"]
+__handlers__ = [
+    (AFK_HANDLER, AFK_GROUP),
+    (AFK_REGEX_HANDLER, AFK_GROUP),
+    (NO_AFK_HANDLER, AFK_GROUP),
+    (AFK_REPLY_HANDLER, AFK_REPLY_GROUP),
+]
