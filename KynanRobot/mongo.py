2527c857dbfde51fabe80ac44ae3fa0189e2f44d
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-07 20:15:19 +0700

    Update mongo.py

--- KynanRobot/mongo.py
+++ KynanRobot/mongo.py
@@ -9,7 +9,7 @@ from KynanRobot.confing import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "KynanRobot"
+MONGO_DB = "Kynan"
 
 
 client = MongoClient()
